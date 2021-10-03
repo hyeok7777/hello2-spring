3825a9aba2fc2a474d92047428c36799157f70d7
오진혁 <hyeok7777@gmail.com>
2021-10-03 17:17:21 +0900

    test(BeanLifeCycleTest) : @PostConstruct @PreDestory

--- core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -24,4 +24,6 @@ public class BeanLifeCycleTest {
             return networkClient;
         }
     }
+
+
 }
--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,14 +1,15 @@
 package hello.core.lifecycle;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
-        System.out.println("생성자 호출, url = "+ url);
-        connect();
-        call("초기화 연결 메시지");
-        disConnect();
+        System.out.println("생성자 호출, url = " + url);
+
     }
 
     public void setUrl(String url) {
@@ -21,11 +22,26 @@ public class NetworkClient {
     }
 
     public void call(String message) {
-        System.out.println("call = " + url + " message = "+message);
+        System.out.println("call = " + url + " message = " + message);
     }
 
     // 서비스 종료 시 호출
     public void disConnect() {
-        System.out.println("closed = "+url);
+        System.out.println("closed = " + url);
+    }
+
+
+    @PostConstruct
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
+        call("destroy");
+        disConnect();
     }
 }
